d7d38714bd949ef92953eff4a837eff49570873b
BIT Student <classroom@bgit.rs>
2018-02-09 16:46:05 +0100

    finished branch test

--- functions/function_practical_4_0209/branch-test/test.js
+++ functions/function_practical_4_0209/branch-test/test.js
@@ -1,1 +1,10 @@
-hcdjdjcdjvhvh
+function hexToRGB(a) {
+    var r, g, b;
+    r = parseInt('0x'+a[1]+a[2]);
+    g = parseInt('0x'+a[3]+a[4]);
+    b = parseInt('0x'+a[5]+a[6]);
+    return 'rgb ('+r+', '+g+', '+b+')';
+
+}
+
+console.log(hexToRGB('#f3a811'));
