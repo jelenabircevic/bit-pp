81c888790a3add4aa94773934a6d2af6885e1b60
jelenabircevic <jelena.bircevic@gmail.com>
2018-03-03 12:13:11 +0100

    movie list second part almost done + needs refactoring

--- Movie List/js/script.js
+++ Movie List/js/script.js
@@ -1,10 +1,20 @@
 var add = document.querySelector('#add');
-var err = document.querySelector('#error');
+var err = document.querySelector('#error1');
+var err2 = document.querySelector('#error2')
+var err3 = document.querySelector('#error3');
+var addp = document.querySelector('#addprogram');
+var addmtp = document.querySelector('#addmtop');
+var selectM = document.querySelector('#movies');
+var selectP = document.querySelector('#programs');
 var movieList = [];
 var inputList = [];
+var programList = [];
+var inputProgramList = [];
 var i = 0;
-var j = 0;
+var l = 0;
+var k = 0;
 var ul = document.querySelector('ul');
+var ulProgram = document.querySelector('#movieProgram');
 //  var span = document.querySelector('#counter');
 var duration = 0;
 
@@ -13,9 +23,33 @@ var duration = 0;
 
 add.addEventListener('click', function (event) {
     var input = fetchData();
+    
     if (!movieList.hasElement(input) && (JSON.stringify(input) != JSON.stringify({}))) {
         addMovie(input);
     }
-    
+    i++ //<---
 
+
+});
+addp.addEventListener('click', function (event) {
+    var input = fetchProgram();
+    if (!programList.hasElement(input) && (JSON.stringify(input) != JSON.stringify({}))) {
+        addProgram(input);
+    } else {
+        err2.style.visibility = "visible";
+        err2.textContent = "Program already exists!";
+    }
+    l++  //<---
+});
+
+addmtp.addEventListener('click', function (event) {
+    var input = fetchAdding();
+    var movie = movieList[input[0]];
+    var program = programList[input[1]];
+    if (program.movieList.hasElement(movie)) {
+        err3.textContent = movie.name + ' is already added to this program. Choose another movie!';
+        err3.style.visibility = 'visible';
+    } else {
+        addMovieToProgram(movie, program);
+    }
 });
--- Movie List/js/utilities.js
+++ Movie List/js/utilities.js
@@ -14,13 +14,36 @@ function validateData(t, d, g) {
     }
     return false;
 }
+function validateProgram(d) {
+    if (!d) {
+        err2.textContent = 'Please enter date!';
+        err2.style.visibility = 'visible';
+    } else {
+        err2.style.visibility = 'hidden';
+        return true;
+    }
+    return false;
+}
+function validateAdding(movie, program) {
+    if (movie == '-' || program == '-') {
+        err3.style.visibility = 'visible';
+        err3.textContent = 'Choose movie and/or program!'
+    } else {
+        return true;
+    }
+    return false;
+}
 function updateList() {
+
     var liNode = document.createElement('li');
     var liText = document.createTextNode(movieList[i].getData());
+
     liNode.appendChild(liText);
     liNode.innerHTML += ' <input type="button" value="X" id=i' + i + '>';
     ul.appendChild(liNode);
+    selectM.appendChild(updateSelect(liText));
     var clearX = document.querySelector('#i' + i);
+
     clearX.addEventListener('click', function (event) {
         var index = parseInt(event.target.id.substring(1));
         duration -= movieList[index].duration;
@@ -29,6 +52,36 @@ function updateList() {
         clearLi(index);
     });
 }
+
+
+function updateProgramList() {
+    var liNode = document.createElement('li');
+    // var liText = document.createTextNode(programList[i].getData());
+    liNode.appendChild(document.createTextNode(programList[k].getData()));
+    ulProgram.appendChild(liNode);
+    selectP.appendChild(updateSelect(document.createTextNode(programList[k].getData())));
+    k++;
+
+
+    // var clearX = document.querySelector('#iP' + i);
+    // todo X on program list
+    // liNode.innerHTML += ' <input type="button" value="X" id=iP' + i + '>';
+    /* clearX.addEventListener('click', function (event) {
+        var index = parseInt(event.target.id.substring(1));
+        // duration -= movieList[index].duration;
+        programList.splice(index, 1);
+        i--;
+        clearLi(index);
+    }); */
+}
+
+function updateSelect(item) {
+    var optionNode = document.createElement('option');
+    optionNode.appendChild(item);
+    return optionNode;
+};
+
+
 function updateCounter() {
     span.textContent = duration + 'min';
 }
@@ -45,20 +98,68 @@ function fetchData() {
     var movieLength = document.querySelector('#length');
     var movieGenre = document.querySelector('#genre');
     if (validateData(movieTitle.value, movieLength.value, movieGenre.value)) {
-        input = new Movie(movieTitle.value, movieLength.value, movieGenre.value);
+        inputList[i] = new Movie(movieTitle.value, movieLength.value, movieGenre.value);
         reset(movieTitle, movieLength, movieGenre);
-        return input;
+        return inputList[i];
     }
     return {};
 }
 
+function fetchProgram() {
+    var programDate = document.querySelector('#date');
+    if (validateProgram(programDate.value)) {
+        inputProgramList[l] = new Program(programDate.value);
+        resetProgram(programDate); //<---
+        return inputProgramList[l];
+    } return {};
+
+}
+
+function fetchAdding() {
+    var movie = document.querySelector('#movies');
+    var program = document.querySelector('#programs');
+    var mIndex;
+    var pIndex;
+    if (validateAdding(movie.value, program.value)) {
+        
+        programList.forEach(function (p, g) {
+            if (p.getData() == program.value) {
+                pIndex = g;
+            }
+        });
+        movieList.forEach(function (m, g) {
+            if (m.getData() == movie.value) {
+                mIndex = g;
+            }
+        });
+        return [mIndex, pIndex];
+
+    }
+    return [];
+}
 function reset(a, b, c) {
     a.value = '';
     b.value = '';
     c.value = '-';
+}
+function resetProgram(a) {  //<---
+    a.value = '';
 }
 function addMovie(obj) {
     movieList.push(obj);
     updateList();
     //TODO add movie to select in program div
 }
+
+function addProgram(obj) {
+    programList.push(obj);
+    updateProgramList();
+
+}
+
+function addMovieToProgram(movie, program) {
+    program.movieList.push(movie);
+    k++;
+}
+
+
